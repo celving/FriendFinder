48705d1920b8cc1cefc79fc37e53c6cac4c23616
Colin <viewtifuljho@hotmail.com>
2019-08-31 17:51:54 -0500

    post method not working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,22 @@
 var friendData = require("../data/friends");
+var path = require("path");
 
 module.exports = function(app) {
+
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
     app.get("/api/friends", function(req, res) {
         res.json(friendData);
     });
 
-    // app.post("/api/friends", function(req, res) {
-        
-    // });
+    app.post("/api/friends", function(req, res) {
+        var surveyResults = req.body;
+        var userScores = surveyResults.scores;
+        console.log(userScores);
+        console.log("hello");
+    });
 
     
 }
